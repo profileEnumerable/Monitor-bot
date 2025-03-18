eefbac3b7b8cba1bcd0ce782e425d46d340599f2
Your Name <you@example.com>
2025-03-18 20:40:33 +0200

    use aspnet project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using AspNet_Air_Alert_Bot.Workers;
+using TdLib;
+using Telegram.Bot;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddHostedService<TelegramWorker>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Workers/TelegramWorker.cs
@@ -0,0 +1,80 @@
+﻿using TdLib;
+using Telegram.Bot;
+using static TdLib.TdApi.MessageContent;
+
+namespace AspNet_Air_Alert_Bot.Workers
+{
+    public class TelegramWorker : BackgroundService
+    {
+        private readonly ILogger<TelegramWorker> _logger;
+
+        public TelegramWorker(ILogger<TelegramWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var botClient = new TelegramBotClient(Environment.GetEnvironmentVariable("TELEGRAM_TOKEN"));
+            await botClient.SendMessage(int.Parse(Environment.GetEnvironmentVariable("CHAT_ID")), "Я працюю !");
+
+            var tdClient = new TdClient();
+
+            tdClient.UpdateReceived += async (sender, update) =>
+            {
+                if (update is TdApi.Update.UpdateAuthorizationState authUpdate)
+                {
+                    await HandleAuth(tdClient, authUpdate.AuthorizationState);
+                }
+            };
+
+            tdClient.UpdateReceived += async (sender, update) =>
+            {
+                if (update is TdApi.Update.UpdateNewMessage newMessageFromChannel)
+                {
+                    if (newMessageFromChannel?.Message.Content is MessageText messageText)
+                    {
+                        string[] keyWords = Environment.GetEnvironmentVariable("KEY_WORDS").Split(",");
+
+                        if (keyWords.Any(keyWord => messageText.Text.Text.Contains(keyWord)))
+                        {
+                            await botClient.SendMessage(int.Parse(Environment.GetEnvironmentVariable("CHAT_ID")), messageText.Text.Text);
+                        }
+                    }
+                }
+            };
+
+            await Task.Delay(-1);
+        }
+
+        static async Task HandleAuth(TdClient client, TdApi.AuthorizationState state)
+        {
+            switch (state)
+            {
+                case TdApi.AuthorizationState.AuthorizationStateWaitTdlibParameters:
+                    Console.WriteLine("Sending TDLib params ...");
+                    await client.ExecuteAsync(new TdApi.SetTdlibParameters
+                    {
+                        UseTestDc = false,
+                        DatabaseDirectory = "tdlib",
+                        UseFileDatabase = true,
+                        UseChatInfoDatabase = true,
+                        UseMessageDatabase = true,
+                        UseSecretChats = false,
+                        ApiId = int.Parse(Environment.GetEnvironmentVariable("API_ID")),
+                        ApiHash = Environment.GetEnvironmentVariable("API_HASH"),
+                        SystemLanguageCode = "en",
+                        DeviceModel = "PC",
+                        ApplicationVersion = "1.0",
+                    });
+                    break;
+
+                case TdApi.AuthorizationState.AuthorizationStateReady:
+                    Console.WriteLine("You're authtorized!");
+                    break;
+            }
+        }
+
+    }
+
+}
